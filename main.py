9d10ce8ff1d2ca625c3122b1663765fc9b2e5a13
sharapovjamshed06-debug <sharapovjamshed06@gmail.com>
2026-09-08 10:29:29 +0500

    Update GEMINI_API_KEY value

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiohttp import web
 from google import genai
 
 BOT_TOKEN = "6482109417:AAGdKMqojdNSFqXLYOWjQeBVMwaYwbTP1JQ"
-GEMINI_API_KEY = "AQ.Ab8RN6Kq-x2X_GRtGKAoUgE19n_ca8aNImwJFiq_17TSA4uovA"
+GEMINI_API_KEY = "AQ.Ab8RN6Kq-x2X_GRtGKAoUgEi9n_ca8aNImwJFiq_17TSA4uovA"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
